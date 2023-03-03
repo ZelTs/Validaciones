6414f43fa41df3820d0bdf41802a30ce29b47339
ZelTs <zeltsmx@outlook.com>
2023-03-03 13:02:08 -0600

    Cambio mensajes en la seccion de formulario

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,13 +1,72 @@
+//Esta funcion se llama cada vez que el usuario sale del input que estaba rellenando
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
+    
     if (validadores[tipoDeInput]) {
       validadores[tipoDeInput](input);
     }
+
+    //Vamos a verificar el input
+    if(input.validity.valid){
+      //Quitamos la clase si es válido los datos ingresados
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+      //Añadimos clase de datos inválidos
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = 
+      mostrarMensajeDeError(tipoDeInput, input);
+    }
   }
+
+  const tiposDeErrores = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+  ];
+
+
+  //Definimos cada mensaje de los inputs en caso de error
+  const mensajesDeError = {
+    //Objeto nombre
+    nombre: {
+      valueMissing: "Este campo no puede estar vacio",
+    },
+    email: {
+      valueMissing: "Este campo no puede estar vacio",
+      typeMismatch: "El correo no es válido",
+    },
+
+    password: {
+      valueMissing: "Este campo no puede estar vacio",
+      patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+    },
+
+    nacimiento: {
+      valueMissing: "Este campo no puede estar vacio",
+      customError: "Debes tener al menos 18 años de edad",
+    },
+  };
   
   const validadores = {
     nacimiento: (input) => validarNacimiento(input),
   };
+
+function mostrarMensajeDeError (tipoDeInput, input){
+  
+  let mensaje = " ";
+
+  tiposDeErrores.forEach( (error) =>{
+    if(input.validity[error]){
+      console.log(tipoDeInput, error);
+      console.log(input.validity[error]);
+      console.log(mensajesDeError[tipoDeInput][error]);
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  });
+  return mensaje;
+}
   
   function validarNacimiento(input) {
     const fechaCliente = new Date(input.value);
